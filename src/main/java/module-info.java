f2f842c6f44634a1ef1ef75f606b13ec71ef3440
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2024-08-29 14:44:59 +0200

    getting GeoLocation of selected city trought API, get request and JSON data extraction

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module org.example {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
+    requires org.apache.httpcomponents.client5.httpclient5;
+    requires org.apache.httpcomponents.core5.httpcore5;
+    requires org.json;
 
     opens org.example;
     opens org.example.controller;
--- src/main/java/org/example/controller/services/GeoLocationService.java
+++ src/main/java/org/example/controller/services/GeoLocationService.java
@@ -2,8 +2,17 @@ package org.example.controller.services;
 
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
 import org.example.model.GeoLocation;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 public class GeoLocationService extends Service {
@@ -20,18 +29,46 @@ public class GeoLocationService extends Service {
             @Override
             protected Object call() throws Exception {
                 try {
-                    System.out.println("uruchomiono task");
-                    System.out.println("API_KEY" + APIKey.getApiKey());
-                    System.out.println("GEO: " + geoLocation.getCity() + " " + geoLocation.getCountry());
-                    BigDecimal lat = new BigDecimal("50.80") ;
-                    geoLocation.setLatitude(lat);
-                    BigDecimal lon = new BigDecimal("15.30") ;
-                    geoLocation.setLongitude(lon);
+//                    System.out.println("uruchomiono task");
+//                    System.out.println("API_KEY" + APIKey.getApiKey());
+//                    System.out.println("GEO: " + geoLocation.getCity() + " " + geoLocation.getCountry());
+//                    BigDecimal lat = new BigDecimal("50.80") ;
+//                    geoLocation.setLatitude(lat);
+//                    BigDecimal lon = new BigDecimal("15.30") ;
+//                    geoLocation.setLongitude(lon);
+                    getGeoLocation(geoLocation, APIKey.getApiKey());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
                 return null;
             }
         };
+    }
+
+    private void getGeoLocation(GeoLocation geoLocation, String apiKey) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+
+            String url = "https://api.openweathermap.org/geo/1.0/direct?q=" + geoLocation.getCity() + "," + geoLocation.getCountry() + "&appid=" + apiKey;
+            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(url)
+                    .build();
+
+            httpclient.execute(httpGet, response -> {
+                System.out.println("GET: " + response.getCode() + " " + response.getReasonPhrase());
+                final HttpEntity entity1 = response.getEntity();
+
+                String retSrc = EntityUtils.toString(entity1);
+
+                JSONArray result1 = new JSONArray(retSrc);
+                JSONObject result2 = result1.getJSONObject(0);
+                geoLocation.setLatitude(result2.getBigDecimal("lat"));
+                geoLocation.setLongitude(result2.getBigDecimal("lon"));
+
+                EntityUtils.consume(entity1);
+                return null;
+            });
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
